aa4ae06b06fcdfd92825576c81cf8db2b0a20451
Toni  Diep <adiep123@gmail.com>
2020-06-11 20:32:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,11 +9,13 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   def self.create_table
-    sql = << -SQL
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER
-    );
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
   end
 end
