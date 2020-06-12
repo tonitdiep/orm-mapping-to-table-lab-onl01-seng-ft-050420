c54d56afe2cab71d32c7a14b4adff3947e5a2f88
Toni  Diep <adiep123@gmail.com>
2020-06-12 02:30:10 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,10 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    # @id = DB[:conn].execute()
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
+  def self.create
+  
   end
 end
