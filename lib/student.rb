f526b014286b91bd763d124ead38ef9ef9e33571
Toni  Diep <adiep123@gmail.com>
2020-06-12 02:14:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,12 +10,28 @@ class Student
   #  with DB[:conn]  
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-      )
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+      SQL
+      DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+      DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
+    # @id = DB[:conn].execute()
   end
 end
